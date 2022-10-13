5c8a33afdf033ffe5123bef79bc39a6d3aec5e66
unknown <flowfxdesignsonline@gmail.com>
2022-10-13 00:27:31 -0500

    search bar is broken

--- .history/src/components/SearchExercises_20221012235629.js
+++ .history/src/components/SearchExercises_20221012235629.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
-import HorizontalScrollbar from './HorizontalScrollBar';
+import HorizontalScrollbar from './horizontalScrollbar';
 
 
 
