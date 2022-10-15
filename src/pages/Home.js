7a468bef2d191530ab4d671b6b16c4517c0e71a9
unknown <flowfxdesignsonline@gmail.com>
2022-10-14 20:38:24 -0500

    Pagination Broken, Api wont display to dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { Box } from '@mui/material';
 
-import HeroBanner from '../components/HeroBanner';
-import SearchExercises from '../components/SearchExercises';
 import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 
 const Home = () => {
-  const [bodyPart, setBodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,18 +1,24 @@
 export const exerciseOptions = {
   method: 'GET',
- 
   headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
+  },
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '51c3b48a2emshcf2ad676421d078p165e20jsncdbcdccf8e04',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
 };
 
 
@@ -20,9 +26,3 @@ export const youtubeOptions = {
 
 
 
-export const fetchData = async (url, options) => {
-    const response = await fetch(url, options);
-    const data = await response.json();
-
-    return data;
-}
